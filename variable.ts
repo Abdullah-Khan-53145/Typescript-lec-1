c8619e6345a4f7b853aa0463299464cb3af0ced1
Abdullah <abdullah.khan53145@gmail.com>
2022-12-10 21:07:08 +0500

    feat: learnt about the any key word that when you you just declare a variable and donot mention the type typescript will asign type any to it which means that for this variable the typescript will not check for the typr which kills the whole perpose of using typescript so yes it is a thing but i have to avoid it

--- variable.ts
+++ variable.ts
@@ -3,5 +3,17 @@ let myNum :number = 6;
 greetings.toLowerCase();
 console.log(greetings)
 
+// number
+let userId  = 334466
+userId.toFixed();
 
+// boolean
+
+let isLoggedIn :boolean = false;
+
+let hero : string;
+function getHero (){
+    return "thor";
+}
+hero =getHero();
 export {}
